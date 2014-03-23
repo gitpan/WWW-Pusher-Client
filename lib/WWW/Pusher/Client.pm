56794ec29ced72bc11c5775c0453345349a15023
Daniel Gempesaw <gempesaw@cpan.org>
2014-03-23 02:56:02 +0000

    Import of GEMPESAW/WWW-Pusher-Client-0.02-TRIAL from CPAN.
    gitpan-cpan-distribution: WWW-Pusher-Client
    gitpan-cpan-version:      0.02-TRIAL
    gitpan-cpan-path:         GEMPESAW/WWW-Pusher-Client-0.02-TRIAL.tar.gz
    gitpan-cpan-author:       GEMPESAW
    gitpan-cpan-maturity:     developer

--- lib/WWW/Pusher/Client.pm
+++ lib/WWW/Pusher/Client.pm
@@ -6,15 +6,21 @@ use warnings;
 use Moo;
 use JSON;
 use AnyEvent::WebSocket::Client;
+use Digest::SHA qw(hmac_sha256_hex);
 
-has 'app_key' => (
+has 'auth_key' => (
     is => 'rw' ,
+    required => 1
+);
+
+has 'secret' => (
+    is => 'rw',
     required => 1
 );
 
 has 'channel' => (
     is => 'rw',
-    required => 1
+    predicate => 'has_channel'
 );
 
 has 'scheme' => (
@@ -40,7 +46,7 @@ has 'ws_url' => (
         my $self = shift;
 
         return $self->{scheme} . $self->{_pusher_base} . $self->{port}
-        . "/app/" . $self->{app_key}
+        . "/app/" . $self->{auth_key}
         . "?protocol=" . $self->{_protocol}
         . "&client=" . $self->{_client_name}
         . "&version=" . $self->{_version}
@@ -48,7 +54,7 @@ has 'ws_url' => (
 );
 
 has 'ws_conn' => (
-    is => 'ro',
+    is => 'rw',
     lazy => 1,
     builder => sub {
         my $self = shift;
@@ -76,39 +82,58 @@ has '_version' => (
     default => '0.001'
 );
 
+has '_socket_id' => (
+    is => 'rw',
+);
+
 
 sub BUILD {
     my $self = shift;
 
-    $self->ws_conn->on(next_message => sub {
-                  $self->subscribe($self->channel);
-              });
-
     $self->ws_conn->on(
-        each_message => sub {
+        next_message => sub {
             my ($conn, $message) = @_;
-            use Data::Dumper; use DDP;
-            p @_;
-            if ($message->body =~ /finish/) {
-                $conn->close();
+            my $body = from_json($message->decoded_body);
+
+            if ($body->{event} eq 'pusher:connection_established') {
+                $self->_socket_id(from_json($body->{data})->{socket_id});
+
+                $self->subscribe($self->channel) if $self->has_channel;
+            }
+            else {
+                die 'Connection error?' . $message->decoded_body;
             }
         });
 }
 
+
 sub subscribe {
     my $self = shift;
+    my $data = {
+        channel => $self->channel
+    };
+    if ($self->channel =~ /^private\-/) {
+        $data->{auth} = $self->_socket_auth($self->channel);
+    }
     $self->ws_conn->send(to_json({
         event => 'pusher:subscribe',
-        data => {
-            channel => $self->channel
-        }
+        data => $data
     }));
 }
 
-sub send {
+sub _socket_auth {
+    my ($self, $channel) = @_;
+    die 'Missing socket_id, sorry...' unless $self->_socket_id;
+
+    my $plainSignature = $self->_socket_id . ':' . $channel;
+    return hmac_sha256_hex($plainSignature, $self->secret);
+}
+
+
+sub trigger {
     my $self = shift;
-    my $message = shift;
     my $event = shift // 'ws update';
+    my $message = shift;
 
     $self->ws_conn->send(to_json({
         event => $event,
@@ -116,6 +141,7 @@ sub send {
         data => $message
     }));
 }
+
 
 1;
 
@@ -131,7 +157,58 @@ WWW::Pusher::Client - Laughably incomplete Perl client for Pusher WS API
 
 =head1 VERSION
 
-version 0.001
+version 0.02
+
+=head1 SYNOPSIS
+
+Pusher is a hosted API for the websocket protocol. WWW::Pusher::Client
+is a laughably incomplete Perl client for their interface. It's really
+only suited for joining one channel in its lifetime - C<bind> and
+C<trigger> both use the most recent channel as defaults.
+
+    use WWW::Pusher::Client;
+    my $pusher =  WWW::Pusher::Client->new(
+        auth_key => $ENV{AUTH_KEY},
+        secret => $ENV{SECRET},
+        channel => 'private-channel'
+    );
+
+    use JSON;
+    $pusher->trigger('my_event', 'this is some data that isn\'t JSON');
+    $pusher->trigger('my_event', to_json({
+        json => 'json also works!'
+    });
+
+=head1 METHODS
+
+=head2 new
+
+Get a client to interact with the Pusher API. You can optionally pass
+in a channel to subscribe to it after the initial connection, or
+subscribe manually later on your own.
+
+    use WWW::Pusher::Client;
+    my $pusher =  WWW::Pusher::Client->new(
+        auth_key => $ENV{AUTH_KEY},  // required
+        secret => $ENV{SECRET},      // required
+        channel => 'default-channel' // optional
+    );
+
+=head2 subscribe
+
+Subscribe to a Pusher channel; currently supporting public and private
+channels, but not presence channels. The authentication for private
+channels is automatically handled for you if your channel name is
+prefixed with 'private-'.
+
+    $pusher->subscribe('pubs-are-easy-to-join');
+    $pusher->subscribe('private-channels-are-supported');
+
+=head2 trigger
+
+Trigger an event & message on the currently subscribed channel.
+
+    $pusher->trigger('my_event', 'this is the message!');
 
 =head1 AUTHOR
 
--- t/client.t
+++ t/client.t
@@ -6,23 +6,36 @@ use Data::Printer;
 use AnyEvent;
 use Test::More;
 
+
 BEGIN {
-    unless (defined $ENV{PUSHER_KEY}) {
+    unless (use_ok('WWW::Pusher::Client')) {
         plan skip_all => "Not running without PUSHER_KEY env var";
         done_testing;
-
         exit 0;
     }
 }
 
+my $fake_auth = '278d425bdf160c739803';
+my $fake_secret = '7ad3773142a6692b25b8';
+
 my $client = WWW::Pusher::Client->new(
-    app_key => $ENV{PUSHER_KEY},
-    channel => 'my_channel'
+    auth_key => $fake_auth,
+    secret => $fake_secret
 );
 
-isa_ok($client, 'WWW::Pusher::Client');
-ok($client->ws_url =~ m/ws\.pusherapp\.com.*app.*protocol.*client.*version/, 'ws_url is formatted properly');
+PUSHER_PROTOCOL: {
+    isa_ok($client, 'WWW::Pusher::Client');
+    ok($client->ws_url =~ m/ws\.pusherapp\.com.*app.*protocol.*client.*version/, 'ws_url is formatted properly');
+}
 
-$client->send('hello');
+SOCKET_AUTH: {
+    $client->_socket_id('1234.1234');
+    my $auth = $client->_socket_auth('private-foobar');
+    cmp_ok($auth, 'eq', '58df8b0c36d6982b82c3ecf6b4662e34fe8c25bba48f5369f135bf843651c3a4', 'fake auth matches');
+}
+
+# use Data::Dumper; use DDP;
+# my $cv = AnyEvent->condvar;
+# $cv->recv;
 
 done_testing;
